b569ef5fd51c33e01a143b8f98921879e5c382c6
Ben Black <weepingwillowben@gmail.com>
2020-12-14 09:18:45 -0500

    updated run job script

--- run_job.py
+++ run_job.py
@@ -13,6 +13,10 @@ def main():
     parser.add_argument('--device', type=str, required=True)
     parser.add_argument('--hyperparameters', type=str, required=True)
     parser.add_argument('--eval-seperate', action="store_true")
+    parser.add_argument('--params_offset1', type=str)
+    parser.add_argument('--params_offset2', type=str)
+    parser.add_argument('--offset1', type=float)
+    parser.add_argument('--offset2', type=float)
 
     args = parser.parse_args()
 
@@ -21,6 +25,15 @@ def main():
 
     weights = torch.load(args.params_path)
     weights = [w.cpu().numpy() for n,w in weights.items()]
+    if args.params_offset1 is not None:
+        offsets = torch.load(args.params_offset1)
+        for w, off in zip(weights, offsets.values()):
+            w += off.detach().cpu().numpy() * args.offset1
+    if args.params_offset2 is not None:
+        offsets = torch.load(args.params_offset2)
+        for w, off in zip(weights, offsets.values()):
+            w += off.detach().cpu().numpy() * args.offset2
+
     agent.set_weights(weights)
 
     eval_agent = None

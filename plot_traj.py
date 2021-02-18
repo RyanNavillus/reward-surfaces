00729e458911924e1f2439a2918617cf72bc1dca
Ben Black <weepingwillowben@gmail.com>
2021-02-17 20:48:57 -0500

    refactored plot_traj.py

--- plot_traj.py
+++ plot_traj.py
@@ -5,13 +5,7 @@ import numpy as np
 import re
 import argparse
 
-def main():
-    parser = argparse.ArgumentParser(description='plot csv data via matplotlib')
-    parser.add_argument('csv_file', type=str)
-    parser.add_argument('--key', type=str, default='episode_rewards')
-
-    args = parser.parse_args()
-    csv_file = args.csv_file
+def plot_traj(csv_file, key="episode_rewards"):
     df = pandas.read_csv(csv_file)
     idxs = np.argsort(df['checkpoint'])
     fig = plt.figure()
@@ -19,6 +13,16 @@ def main():
     ax.plot(df['checkpoint'][idxs], df[args.key][idxs])
     out_fname = "vis/"+"".join([c for c in csv_file if re.match(r'\w', c)]) + args.key
     plt.savefig(out_fname)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='plot csv data via matplotlib')
+    parser.add_argument('csv_file', type=str)
+    parser.add_argument('--key', type=str, default='episode_rewards')
+
+    args = parser.parse_args()
+
+    plot_traj(args.csv_file, key=args.key)
 
 if __name__ == "__main__":
     main()

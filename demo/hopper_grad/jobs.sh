1f28ee68d0c40d2027268d1f926d4faa30d9288d
Ben Black <weepingwillowben@gmail.com>
2021-02-19 16:14:13 -0500

    added demo

---
+++ demo/hopper_grad/jobs.sh
@@ -0,0 +1,9 @@
+python eval_tradj.py train_results/bullet/hopper/ 0010000 generated_dirs/hopper_grad --device=cpu
+python eval_tradj.py train_results/bullet/hopper/ 0040000 generated_dirs/hopper_grad --device=cpu
+python eval_tradj.py train_results/bullet/hopper/ 0080000 generated_dirs/hopper_grad --device=cpu
+python eval_tradj.py train_results/bullet/hopper/ 0160000 generated_dirs/hopper_grad --device=cpu
+python eval_tradj.py train_results/bullet/hopper/ 0320000 generated_dirs/hopper_grad --device=cpu
+python eval_tradj.py train_results/bullet/hopper/ 0640000 generated_dirs/hopper_grad --device=cpu
+python eval_tradj.py train_results/bullet/hopper/ 1280000 generated_dirs/hopper_grad --device=cpu
+python eval_tradj.py train_results/bullet/hopper/ 2560000 generated_dirs/hopper_grad --device=cpu
+python eval_tradj.py train_results/bullet/hopper/ 4500000 generated_dirs/hopper_grad --device=cpu

8f87298ff39e87f5454c39d0d6b6499a3c4a314f
Ben Black <weepingwillowben@gmail.com>
2021-03-05 10:20:49 -0500

    added gif scripts

---
+++ scripts/generate_gif_jobs.py
@@ -0,0 +1,73 @@
+import torch
+import numpy as np
+from reward_surfaces.experiments import generate_plane_data
+import json
+import subprocess
+import argparse
+from reward_surfaces.utils.surface_utils import filter_normalize
+from pathlib import Path
+import random
+import os
+
+def main():
+    parser = argparse.ArgumentParser(description='generate jobs for plane')
+    parser.add_argument('train_dir', type=str)
+    parser.add_argument('out_dir', type=str)
+    parser.add_argument('--device', type=str, default='cpu')
+    parser.add_argument('--num-steps', type=int)
+    parser.add_argument('--num-episodes', type=int)
+    parser.add_argument('--magnitude', type=float, default=0.2, help="scales directions by given amount")
+    parser.add_argument('--grid-size', type=int, default=5)
+
+    args = parser.parse_args()
+
+    generate_gif_jobs(
+        args.train_dir,
+        args.out_dir,
+        num_steps=args.num_steps,
+        num_episodes=args.num_episodes,
+        device=args.device,
+        magnitude=args.magnitude,
+        grid_size=args.grid_size,
+    )
+
+
+def generate_gif_jobs(train_dir, out_dir, num_steps=None, num_episodes=None, device="cpu", seed=None, grid_size=5, magnitude=0.2):
+    train_dir = Path(train_dir)
+    out_dir = Path(out_dir)
+
+    if seed is None:
+        seed = random.randint(0,2**31)
+
+    os.mkdir(out_dir)
+
+    all_gen_dirs = []
+    for p_str in sorted(os.listdir(train_dir)):
+        checkpoint_param_fname = train_dir / p_str / "parameters.th"
+        if os.path.exists(checkpoint_param_fname):
+            trained_checkpoint = train_dir / p_str
+            generated_dir = out_dir / p_str
+
+
+            train_info = json.load(open(train_dir / "info.json"))
+
+            # reseeds so that random generated directions are the same every time
+            np.random.seed(seed)
+
+            checkpoint_params = [v.cpu().detach().numpy() for v in torch.load(checkpoint_param_fname,map_location=torch.device('cpu')).values()]
+            dir1 = [filter_normalize(v) for v in checkpoint_params]
+            dir2 = [filter_normalize(v) for v in checkpoint_params]
+
+            generate_plane_data(trained_checkpoint, generated_dir, dir1, dir2, train_info, num_steps=num_steps, num_episodes=num_episodes, device=device)
+
+            all_gen_dirs.append(generated_dir)
+
+    # concatenate all jobs
+    all_job_paths = "".join([open(dir/"jobs.sh").read() for dir in all_gen_dirs])
+    multiproc_job_paths = "\n".join([f"python scripts/run_jobs_multiproc.py {dir/'jobs.sh'}" for dir in all_gen_dirs])
+    open(out_dir/"all_jobs.sh",'w').write(all_job_paths)
+    open(out_dir/"multiproc_jobs.sh",'w').write(multiproc_job_paths)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/plot_gif.py
@@ -0,0 +1,49 @@
+import argparse
+from reward_surfaces.plotting import plot_plane
+import os
+from pathlib import Path
+from reward_surfaces.utils.job_results_to_csv import job_results_to_csv
+import shutil
+import numpy as np
+import pandas
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='generate jobs for plane')
+    parser.add_argument('gif_data_folder', type=str)
+    parser.add_argument('--outname', type=str, help="if specified, outputs file with this name (extension added onto name)")
+    parser.add_argument('--key', type=str, default="episode_rewards", help="key in csv file to plot")
+    parser.add_argument('--type', type=str, default="mesh", help="plot type. Possible types are: [all, mesh, vtp, heat, contour, contourf]")
+
+    args = parser.parse_args()
+
+    frames_dir = "_frames/"
+    if os.path.exists(frames_dir):
+        shutil.rmtree(frames_dir)
+    os.mkdir(frames_dir)
+
+    gif_data_folder = Path(args.gif_data_folder)
+
+    checkpoints = []
+    for checkpoint in sorted(os.listdir(gif_data_folder)):
+        check_path = gif_data_folder / checkpoint
+        if os.path.isdir(check_path):
+            checkpoints.append(checkpoint)
+
+    for checkpoint in checkpoints:
+        job_results_to_csv(gif_data_folder / checkpoint)
+
+    base_mag = None
+    factor = 2
+    frame_idx = 0
+    for checkpoint in checkpoints:
+        csv_path = gif_data_folder / checkpoint / "results.csv"
+        m = np.max(pandas.read_csv(csv_path)[args.key])
+        if base_mag is None:
+            base_mag = m
+        else:
+            while m > base_mag * factor:
+                base_mag = base_mag * factor
+
+            fname = plot_plane(str(csv_path), str(frames_dir+checkpoint), args.key, args.type, show=False)
+            os.rename(fname, f"{frames_dir}{frame_idx:05}.png")
+            frame_idx += 1

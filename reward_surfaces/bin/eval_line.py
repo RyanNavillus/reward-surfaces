2d835ef0913cfb26bf7fb59f1ee9083ed96d7d42
Ben Black <weepingwillowben@gmail.com>
2021-04-02 21:37:34 -0400

    added line segment experiment

---
+++ reward_surfaces/bin/eval_line.py
@@ -0,0 +1,48 @@
+import argparse
+import json
+import os
+import torch
+from reward_surfaces.utils.surface_utils import readz
+from pathlib import Path, PurePath
+from reward_surfaces.algorithms import evaluate
+from reward_surfaces.agents import make_agent
+
+
+bigint = 1000000000000
+def main():
+    parser = argparse.ArgumentParser(description='run a particular evaluation job')
+    parser.add_argument('params', type=str)
+    parser.add_argument('dir', type=str, help="direction to search for threshold")
+    parser.add_argument('outputfile', type=str)
+    parser.add_argument('--num-steps', type=int, default=bigint)
+    parser.add_argument('--num-episodes', type=int, default=bigint)
+    parser.add_argument('--device', type=str, default="cpu")
+    parser.add_argument('--length', type=int, default=5)
+    parser.add_argument('--max-magnitude', type=float, default=0.1)
+
+    args = parser.parse_args()
+
+    info = json.load(open(PurePath(args.outputfile).parent.parent / "info.json"))
+    checkpoint = os.path.basename(args.outputfile)
+
+    device = args.device
+    agent = make_agent(info['agent_name'], info['env'], device, info['hyperparameters'])
+
+    params = [v.cpu().detach().numpy() for v in torch.load(args.params,map_location=torch.device('cpu')).values()]
+    dir = readz(args.dir)
+
+    for i in range(args.length):
+        mm = args.max_magnitude
+        weights = [p+d*mm for p,d in zip(params, dir)]
+        agent.set_weights(weights)
+        evaluator = agent.evaluator()
+        eval_results = evaluate(evaluator, args.num_episodes, args.num_steps)
+        eval_results['checkpoint'] = checkpoint
+        out_fname = f"{args.outputfile},{i}.json"
+        eval_results["offset"] = i*mm/args.length
+
+        with open(out_fname, 'w') as file:
+            file.write(json.dumps(eval_results))
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/eval_line_segment.py
@@ -0,0 +1,59 @@
+import argparse
+import json
+import os
+import torch
+from reward_surfaces.utils.surface_utils import readz
+from pathlib import Path
+from reward_surfaces.algorithms import search
+from reward_surfaces.runners.run_jobs_multiproc import run_job_list_list
+from reward_surfaces.utils.job_results_to_csv import job_results_to_csv
+
+
+bigint = 1000000000000
+def main():
+    parser = argparse.ArgumentParser(description='run a particular evaluation job')
+    parser.add_argument('train_dir', type=str)
+    parser.add_argument('grads_dir', type=str, help="direction to search for threshold")
+    parser.add_argument('output_dir', type=str)
+    parser.add_argument('--num-steps', type=int, default=bigint)
+    parser.add_argument('--num-episodes', type=int, default=bigint)
+    parser.add_argument('--device', type=str, default="cpu")
+    parser.add_argument('--length', type=int, default=5)
+    parser.add_argument('--max-magnitude', type=float, default=0.1)
+
+    args = parser.parse_args()
+
+    assert args.num_steps != bigint or args.num_episodes != bigint, "one of num_steps or num_episodes must be specified"
+
+    out_path = Path(args.output_dir)
+    train_path = Path(args.train_dir)
+    grad_path = Path(args.grads_dir)
+    os.mkdir(out_path)
+    os.mkdir(out_path/"results")
+
+    checkpoints = sorted([checkpoint for checkpoint in os.listdir(train_path) if os.path.isdir(train_path/checkpoint)])
+    commands = []
+    for checkpoint in checkpoints:
+        params = train_path/checkpoint/"parameters.th"
+        grad = grad_path/checkpoint/"grad.npz"
+        out_fname = str(out_path/"results"/checkpoint)
+        command = f"python -m reward_surfaces.bin.eval_line {params} {grad} {out_fname} --num-steps={args.num_steps} --num-episodes={args.num_episodes} --device {args.device} --length {args.length} --max-magnitude {args.max_magnitude}"
+        commands.append(command)
+
+    info = json.load(open(train_path/"info.json"))
+    info['num_steps'] = args.num_steps
+    info['num_episodes'] = args.num_episodes
+    info['draw_length'] = args.length
+    info['draw_max_magnitude'] = args.max_magnitude
+    info['grad_loc'] = args.grads_dir
+    info['params_loc'] = args.train_dir
+
+    json.dump(info, open(out_path/"info.json",'w'))
+
+    with open(out_path/"jobs.sh", 'w') as file:
+        file.write("\n".join(commands))
+    # run_job_list_list(commands)
+    # job_results_to_csv(out_path)
+
+if __name__ == "__main__":
+    main()

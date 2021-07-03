cb44a713d28e5c39c0b62813dc460f129650fc9c
Ryan Sullivan <ryanpnavillus@gmail.com>
2021-07-03 16:15:16 -0400

    Fix scripts

--- plot.sh
+++ plot.sh
@@ -8,4 +8,4 @@ export PYTHONPATH=/home/ryansullivan/Documents/SwarmLabs/reward-surfaces
 python scripts/generate_plane_jobs.py --grid-size=31 --magnitude=1.0 --num-steps=200000 "./runs/${checkpoint_name}_checkpoints/best" "./runs/${checkpoint_name}_eig_vecs_plane/"
 python scripts/run_jobs_multiproc.py "./runs/${checkpoint_name}_eig_vecs_plane/jobs.sh"
 python scripts/job_results_to_csv.py "./runs/${checkpoint_name}_eig_vecs_plane/"
-python scripts/plot_plane.py "./runs/${checkpoint_name}_eig_vecs_plane/results.csv" --outname="${checkpoint_name}_curvature_plot.png"
+python scripts/plot_plane.py "./runs/${checkpoint_name}_eig_vecs_plane/results.csv" --outname="./runs/${checkpoint_name}_curvature_plot.png"

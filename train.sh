e6a2a531078673f5b84892482236aced9a3e084f
Ryan Sullivan <ryanpnavillus@gmail.com>
2021-07-03 20:00:04 -0400

    Add hypers

--- train.sh
+++ train.sh
@@ -4,6 +4,6 @@ checkpoint_name=$2
 echo $env_name
 echo $checkpoint_name
 
-export PYTHONPATH=/home/ryansullivan/Documents/SwarmLabs/reward-surfaces
-python scripts/train_agent.py "./runs/${checkpoint_name}_checkpoints" 1000000 SB3_ON "$env_name" cuda '{"ALGO": "PPO",
-"num_envs": 8}' --save_freq=10000
+export PYTHONPATH=/home/ryan/reward-surfaces
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ryan/.mujoco/mujoco200/bin
+python3 scripts/train_agent.py "./runs/${checkpoint_name}_checkpoints" 1000000 SB3_ON "$env_name" cuda '{"ALGO": "PPO", "num_envs": 8}' --save_freq=10000

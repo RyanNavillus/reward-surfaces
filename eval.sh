9a139c7043c8be5e660883f7822a1c53ecd655bd
Ryan Sullivan <ryanpnavillus@gmail.com>
2021-07-05 19:47:18 -0400

    Remove scripts from source control

--- eval.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-env_name=$1
-checkpoint_name=$2
-echo $env_name
-echo $checkpoint_name
-
-export PYTHONPATH=/home/ryan_sullivan/reward-surfaces
-python scripts/generate_eval_jobs.py --num-steps=1000000 --checkpoint="best" "./runs/${checkpoint_name}_checkpoints" "./runs/${checkpoint_name}_eig_vecs/"
-python scripts/run_jobs_multiproc.py "./runs/${checkpoint_name}_eig_vecs/jobs.sh"
--- train.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-env_name=$1
-checkpoint_name=$2
-echo $env_name
-echo $checkpoint_name
-
-export PYTHONPATH=/home/ryan_sullivan/reward-surfaces
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ryan/.mujoco/mujoco200/bin
-python3 scripts/train_agent.py "./runs/${checkpoint_name}_checkpoints" SB3_ON "$env_name" cuda '{"ALGO": "PPO"}' --save_freq=1000000

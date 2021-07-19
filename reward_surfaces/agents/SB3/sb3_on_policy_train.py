Fix vector normalization loading and improve plots

reward_surfaces/agents/SB3/sb3_on_policy_train.py
@@ -201,22 +201,23 @@ class EvalParamCallback(EvalCallback):
                 if self.best_model_save_path is not None:
                     self.model.save(os.path.join(self.best_model_save_path, "checkpoint"))
                 self.best_mean_reward = mean_reward
+                os.makedirs(self.log_path, exist_ok=True)
+                save_path = os.path.join(self.log_path, "checkpoint")
+                self.save_folders.append(save_path)
+                model_parameters = self.model.policy.state_dict()
+                grads = OrderedDict([(name, param.grad) for name, param in model_parameters.items()])
+                torch.save(model_parameters, os.path.join(self.log_path, "parameters.th"))
+                torch.save(grads, os.path.join(self.log_path, "grads.th"))
+
+                if self.old_params is not None:
+                    delta = OrderedDict([
+                        (name, param - old_param) for old_param, (name, param) in zip(self.old_params, model_parameters.items())
+                    ])
+                    torch.save(delta, os.path.join(self.log_path, "prev_step.th"))
+
                 # Trigger callback if needed
                 if self.callback is not None:
                     return self._on_event()
-            os.makedirs(self.log_path, exist_ok=True)
-            save_path = os.path.join(self.log_path, "checkpoint")
-            self.save_folders.append(save_path)
-            model_parameters = self.model.policy.state_dict()
-            grads = OrderedDict([(name, param.grad) for name, param in model_parameters.items()])
-            torch.save(model_parameters, os.path.join(self.log_path, "parameters.th"))
-            torch.save(grads, os.path.join(self.log_path, "grads.th"))
-
-            if self.old_params is not None:
-                delta = OrderedDict([
-                    (name, param - old_param) for old_param, (name, param) in zip(self.old_params, model_parameters.items())
-                ])
-                torch.save(delta, os.path.join(self.log_path, "prev_step.th"))
 
         return True
 
@@ -257,7 +258,6 @@ class SB3OnPolicyTrainer:
         self.env_fn = env_fn
         self.eval_env_fn = eval_env_fn
         self.algorithm = sb3_algorithm
-        print(self.algorithm)
         self.device = sb3_algorithm.device
 
         # Callbacks
@@ -286,12 +286,11 @@ class SB3OnPolicyTrainer:
             # Account for the number of parallel environments
             self.eval_freq = max(self.eval_freq // self.n_envs, 1)
 
-            print("Creating test environment")
-
-            # save_vec_normalize = SaveVecNormalizeCallback(save_freq=1, save_path=f"{save_dir}/{self.env_id}")
-            eval_env = self.eval_env_fn
+            save_vec_normalize = SaveVecNormalizeCallback(save_freq=1, save_path=save_dir)
+            eval_env = self.eval_env_fn()
             eval_callback = EvalParamCallback(
                 eval_env,
+                callback_on_new_best=save_vec_normalize,
                 best_model_save_path=save_dir + '/best/',
                 n_eval_episodes=self.n_eval_episodes,
                 log_path=save_dir + '/best/',
@@ -327,7 +326,7 @@ class SB3OnPolicyTrainer:
             self.load_weights(fname)
 
     def load_weights(self, load_file):
-        self.algorithm = type(self.algorithm).load(load_file, self.env_fn(), self.device)
+        self.algorithm = type(self.algorithm).load(load_file, self.eval_env_fn(), self.device)
 
     def save_weights(self, save_file):
         self.algorithm.save(save_file)
@@ -341,7 +340,7 @@ class SB3OnPolicyTrainer:
         return buffer_stats
 
     def evaluator(self, eval_trainer=None):
-        return OnPolicyEvaluator(self.env_fn(), self.algorithm.gamma, self.algorithm, eval_trainer)
+        return OnPolicyEvaluator(self.eval_env_fn(), self.algorithm.gamma, self.algorithm, eval_trainer)
 
     def action_evalutor(self):
         return self.algorithm
@@ -364,7 +363,7 @@ class OffPolicyEvaluator(OnPolicyEvaluator):
 
 class SB3OffPolicyTrainer(SB3OnPolicyTrainer):
     def evaluator(self, eval_trainer=None):
-        return OffPolicyEvaluator(self.env_fn(), self.algorithm.gamma, self.algorithm, eval_trainer)
+        return OffPolicyEvaluator(self.eval_env_fn(), self.algorithm.gamma, self.algorithm, eval_trainer)
 
 
 class HERPolicyEvaluator(OnPolicyEvaluator):

reward_surfaces/plotting/plot_plane.py
@@ -62,9 +62,29 @@ def plot_2d_contour(x_coords, y_coords, z_values, base_name, vmin=0.1, vmax=10, 
     if plot_type == 'all' or plot_type == 'mesh':
         fig = plt.figure()
         ax = Axes3D(fig)
-        surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-        ax.plot([0.], [0.], [vmax], markerfacecolor='k', markeredgecolor='k', marker='o', markersize=5, alpha=1.0)
-        ax.plot([0.], [0.], [vmin], markerfacecolor='k', markeredgecolor='k', marker='o', markersize=5, alpha=1.0)
+        Z = np.clip(Z, np.max(Z)-10, np.max(Z))
+
+        # Plot surface
+        surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False, zorder=5)
+
+        # Plot min/max markers
+        ax.plot([0.], [0.], [np.max(Z)], markerfacecolor='k', markeredgecolor='k', marker="_", markersize=5, alpha=1.0,
+                zorder=6)
+        ax.plot([0.], [0.], [np.min(Z)], markerfacecolor='k', markeredgecolor='k', marker="_", markersize=5, alpha=1.0,
+                zorder=1)
+
+        # Plot center line above surface
+        Z_range = abs(np.max(Z) - np.min(Z))
+        zline_above = np.linspace(Z[len(Z) // 2][len(Z[0]) // 2], np.max(Z) + (Z_range * 0.1), 4)
+        xline_above = 0 * zline_above
+        yline_above = 0 * zline_above
+        ax.plot3D(xline_above, yline_above, zline_above, 'black', zorder=10)
+
+        # Plot center line below surface
+        zline_below = np.linspace(Z[len(Z) // 2][len(Z[0]) // 2], np.min(Z) - (Z_range * 0.1), 4)
+        xline_below = 0 * zline_below
+        yline_below = 0 * zline_below
+        ax.plot3D(xline_below, yline_below, zline_below, 'black', zorder=0)
 
         def get_axes(tick_count, scale):
             ticks = []
@@ -84,14 +104,6 @@ def plot_2d_contour(x_coords, y_coords, z_values, base_name, vmin=0.1, vmax=10, 
                 current_label += label_rate
             return ticks, tick_labels
 
-        #print(get_axes(9, dir1_scale))
-        #x_ticks, x_labels = get_axes(9, dir1_scale)
-        #y_ticks, y_labels = get_axes(9, dir2_scale)
-        #ax.set_xticks(x_ticks)
-        #ax.set_yticks(y_ticks)
-        #ax.set_xticklabels(x_labels)
-        #ax.set_yticklabels(y_labels)
-        print(vmin, vmax)
         fig.colorbar(surf, shrink=0.5, aspect=5)
         out_fname = base_name + '_3dsurface.png'
         fig.savefig(out_fname, dpi=300,
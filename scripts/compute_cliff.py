97d343c908fb1d5bf9eee431be6f1672ca706744
Ryan Sullivan <ryanpnavillus@gmail.com>
2021-10-02 13:06:48 -0400

    Global cliff metric

--- scripts/compute_cliff.py
+++ scripts/compute_cliff.py
@@ -25,7 +25,7 @@ def choose_color(is_cliff, index):
         return standard_colors[index % 5]
 
 
-def print_cliff_values(csv_fname, outname=None, key_name="episode_rewards", cliff_ratio=0.1):
+def print_cliff_values(csv_fname, outname=None, key_name="episode_rewards", title=None, cliff_ratio=0.1):
     default_outname = "vis/" + "".join([c for c in csv_fname if re.match(r'\w', c)]) + key_name
     outname = outname if outname is not None else default_outname
     outname = outname + "_cliff"
@@ -35,7 +35,9 @@ def print_cliff_values(csv_fname, outname=None, key_name="episode_rewards", clif
     xvals = (data['dim0'].values)
     yvals = (data['offset'].values)
     zvals = (data[key_name].values)#.reshape(dsize,dsize)
-
+    global_min = min(zvals)
+    global_max = max(zvals)
+    global_range = global_max - global_min
 
     x_size = len(np.unique(xvals))
     y_size = len(np.unique(yvals))
@@ -56,27 +58,49 @@ def print_cliff_values(csv_fname, outname=None, key_name="episode_rewards", clif
         # Compute metric
         is_cliff = False
         # First 10 points are in 1 high-res step
-        if sorted_z[9] <= cliff_ratio * sorted_z[0]:
+        if (sorted_z[0] - sorted_z[9]) / global_range > cliff_ratio:
             is_cliff = True
-        second_last_val = sorted_z[0]
+        second_last_val = sorted_z[8]
         last_val = sorted_z[9]
         for z in sorted_z[10:]:
-            if z <= cliff_ratio * last_val or z <= cliff_ratio * second_last_val:
+            short_drop = last_val - z
+            long_drop = second_last_val - z
+            if short_drop / global_range > cliff_ratio or long_drop / global_range > cliff_ratio:
                 is_cliff = True
             second_last_val = last_val
             last_val = z
 
+        #max_cliff = 1
+        ## First 10 points are in 1 high-res step
+        #percent_remaining = sorted_z[9] / sorted_z[0]
+        #if percent_remaining < 1:
+        #    max_cliff = min(max_cliff, percent_remaining)
+        #second_last_val = sorted_z[8]
+        #last_val = sorted_z[9]
+        #for z in sorted_z[10:]:
+        #    percent_remaining = z / last_val
+        #    if percent_remaining < 1:
+        #        max_cliff = min(max_cliff, percent_remaining)
+
+        #    percent_remaining = z / second_last_val
+        #    if percent_remaining < 1:
+        #        max_cliff = min(max_cliff, percent_remaining)
+        #    second_last_val = last_val
+        #    last_val = z
+
+
         #print(sorted_z)
-        print("\t", x_dims[i], is_cliff)
+        if not is_cliff:
+            print("\t", x_dims[i], is_cliff)
         color = choose_color(is_cliff, i)
         ax.plot(xvals[boolmask][indexes], yvals[boolmask][indexes], zvals[boolmask][indexes], color=color)#, rstride=1, cstride=1)
 
-    ax.set_title(csv_fname)
+    ax.set_title(title)
     ax.set_xlabel('Training time')
     ax.set_ylabel('Gradient step size')
     ax.set_zlabel('Rewards')
-    fig.savefig(outname, dpi=300,
-                bbox_inches='tight', format='png')
+    fig.savefig(outname, dpi=300, bbox_inches='tight', format='png')
+    plt.close()
 
 
 if __name__ == "__main__":
@@ -93,7 +117,7 @@ if __name__ == "__main__":
         name = csv_file.split("/")[-2]
         print(name)
         path = "/".join(csv_file.split("/")[:-2])
-        print_cliff_values(csv_file, outname=path + "/" + name + "_lines", cliff_ratio=args.ratio)
+        print_cliff_values(csv_file, outname=path + "/" + name + "_lines", title=name, cliff_ratio=args.ratio)
         print()
 
 

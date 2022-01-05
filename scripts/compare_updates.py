ee3da624d8cb8fe78bfca63520e05419d112691b
Ryan Sullivan <ryanpnavillus@gmail.com>
2022-01-05 18:28:22 -0500

    Complete main bidy of vpg experiment

--- scripts/compare_updates.py
+++ scripts/compare_updates.py
@@ -7,18 +7,9 @@ from reward_surfaces.algorithms import evaluate
 
 parser = argparse.ArgumentParser(description='Train an agent and keep track of important information.')
 parser.add_argument('--device', type=str, default="cuda", help="Device used for training ('cpu' or 'cuda')")
-parser.add_argument('--checkpoint', type=int, help="Specific checkpoint to test")
-parser.add_argument('--name', type=str, help="Name of checkpoint")
-parser.add_argument('--env', type=str, help="Environment name")
-parser.add_argument('--checkpoint-file', type=str, help="File of checkpoint to test")
+parser.add_argument('--cliff-file', type=str, help="File of cliff checkpoints to test")
+parser.add_argument('--noncliff-file', type=str, help="File of non-cliff checkpoints to test")
 args = parser.parse_args()
-assert not args.checkpoint or not args.checkpoint_file, "must choose either one checkpoint or checkpoint file"
-if args.checkpoint:
-    assert args.name and args.env, "Must specify an agent name when using a single checkpoint"
-
-
-checkpoint_name = args.name
-environment = args.env
 
 
 def compare_a2c_ppo(environment, agent_name, checkpoint):
@@ -29,33 +20,59 @@ def compare_a2c_ppo(environment, agent_name, checkpoint):
         "trained_steps": int(f"{checkpoint}"),
     }
 
-    agent, steps = make_agent("SB3_ON", environment, directory, json.loads('{"ALGO": "A2C"}'), eval_freq=4096, n_eval_episodes=100, pretraining=pretraining, device=args.device)
+    # Baseline
+    agent, steps = make_agent("SB3_ON", environment, directory, json.loads('{"ALGO": "A2C"}'), eval_freq=100000000, n_eval_episodes=1, pretraining=pretraining, device=args.device)
     agent.load_weights(f"{directory}/{checkpoint}/checkpoint.zip")
     #agent.set_weights(weights)
     evaluator = agent.evaluator()
-    print(evaluate(evaluator, 100, 100000000))
-    agent.train(1, f"./runs/vpg/{agent_name}/{checkpoint}", save_freq=10000)
-    print(evaluate(evaluator, 100, 100000000))
+    baseline = evaluate(evaluator, 10, 100000000)
+    print("Baseline: ", baseline["episode_rewards"])
 
-    agent, steps = make_agent("SB3_ON", environment, directory, json.loads('{"ALGO": "PPO"}'), eval_freq=4096, n_eval_episodes=1, pretraining=pretraining, device=args.device)
+    # One step A2C
+    agent.train(1, f"./runs/vpg/{agent_name}/{checkpoint}", save_freq=10000)
+    a2c = evaluate(evaluator, 10, 100000000)
+    print("A2C: ", a2c["episode_rewards"])
+
+    # One step PPO
+    agent, steps = make_agent("SB3_ON", environment, directory, json.loads('{"ALGO": "PPO"}'), eval_freq=100000000, n_eval_episodes=1, pretraining=pretraining, device=args.device)
     agent.load_weights(f"{directory}/{checkpoint}/checkpoint.zip")
     agent.train(1, f"./runs/vpg/{agent_name}/{checkpoint}", save_freq=10000)
+    ppo = evaluate(evaluator, 10, 100000000)
+    print("PPO: ", ppo["episode_rewards"])
 
+    # Calculate statistics
+    baseline_reward = baseline["episode_rewards"]
+    a2c_reward = a2c["episode_rewards"]
+    ppo_reward = ppo["episode_rewards"]
+    ppo_percent = ppo_reward / baseline_reward
+    a2c_percent = a2c_reward / baseline_reward
+    return ppo_percent, a2c_percent
 
-if args.checkpoint:
-    compare_a2c_ppo(args.env, args.name, args.checkpoint)
-else:
+# Cliff test
+def test_checkpoints(checkpoint_filename):
     checkpoints = []
-    with open(args.checkpoint_file, "r") as checkpoint_file:
+    with open(checkpoint_filename, "r") as checkpoint_file:
         for line in checkpoint_file:
             words = line.split(" ")
             assert len(words) == 3, f"Incorrectly formatted checkpoints file (Expected 3 words per line, found {len(words)}"
             env, name, number = words[0], words[1], words[2]
             checkpoints.append((env, name, number))
-
+ 
+    ppo_percents = []
+    a2c_percents = []
     for checkpoint in checkpoints:
         env, name, number = checkpoint
-        compare_a2c_ppo(env, name, number.strip())
+        ppo_percent, a2c_percent = compare_a2c_ppo(env, name, number.strip())
+        ppo_percents.append(ppo_percent)
+        a2c_percents.append(a2c_percent)
+    ppo_avg_percent = sum(ppo_percents) / len(ppo_percents)
+    a2c_avg_percent = sum(a2c_percents) / len(a2c_percents)
+    return ppo_avg_percent, a2c_avg_percent
+
+cliff_ppo, cliff_a2c = test_checkpoints(args.cliff_file)
+noncliff_ppo, noncliff_a2c = test_checkpoints(args.noncliff_file)
+print(noncliff_ppo, cliff_ppo)
+print(noncliff_a2c, cliff_a2c)
 
 
 # agent, steps = make_agent("SB3_ON", "InvertedDoublePendulum-v2", "./runs/vpg_test", json.loads('{"ALGO": "PPO", "n_timesteps": 100000}'), eval_freq=10000, device="cuda")

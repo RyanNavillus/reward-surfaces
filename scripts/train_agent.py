bb00fc3ce0e69fc9abaab453bc377e550fe5c04d
Ryan Sullivan <ryanpnavillus@gmail.com>
2022-02-14 15:55:45 -0500

    Fix eval_freq arg

--- scripts/train_agent.py
+++ scripts/train_agent.py
@@ -48,7 +48,7 @@ def main():
     # trainer = SB3HerPolicyTrainer(robo_env_fn,HER("MlpPolicy",robo_env_fn(),model_class=TD3,device="cpu",max_episode_length=100))
     print(args.resume)
     agent, steps = make_agent(args.agent_name, args.env, args.save_dir, json.loads(args.hyperparameters),
-                              pretraining=pretraining, device=args.device)
+                              pretraining=pretraining, device=args.device, eval_freq=args.eval_freq)
 
     os.makedirs(args.save_dir, exist_ok=True)
 
@@ -62,7 +62,7 @@ def main():
     with open(run_info_fname, 'w') as file:
         file.write(json.dumps(run_info, indent=4))
 
-    agent.train(steps, args.save_dir, save_freq=args.save_freq, eval_freq=args.eval_freq)
+    agent.train(steps, args.save_dir, save_freq=args.save_freq)
 
 
 if __name__ == "__main__":
